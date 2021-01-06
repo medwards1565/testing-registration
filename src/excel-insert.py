88186963d8b0a0702aefbcfa77e8bec12ff8d445
Michael Edwards <medwards1565@gmail.com>
2021-01-06 15:28:23 -0800

    Changed to functions.

--- src/excel-insert.py
+++ src/excel-insert.py
@@ -4,37 +4,49 @@ Created on Dec 22, 2020
 @author: medwa
 '''
 
-# Write a function that accepts an excel file as a parameter (example is in test-registration/resources/) and copies 
-# Scheduled Time (column c), First Name (column d), Last Name (column e), Department (column f), Date of Birth (column h) 
-# into a new tab.
-
 import openpyxl
-from builtins import str
 
-# get our excel file
-xlfile = 'C:\\Users\\medwa\\Desktop\\xlfolder\\test.xlsx'
+qrcode = 'michael,edwards,1986-04-26,162222c615e70,therookie'
 
-# load workbook
-print('test-registration: Loading workbook ' + xlfile)
-wb = openpyxl.load_workbook(xlfile)
+# xlfile is path to .xlsx file, will eventually be read from scanner
+# tempsheet is name of sheet that will be created and manipulated by program
+# mastersheet is name of sheet that needs to be parsed
+def copy_to_new_sheet(xlfile, tempsheet, mastersheet):
+    # load workbook
+    print('test-registration(copy_to_new_sheet): Loading workbook ' + xlfile)
+    wb = openpyxl.load_workbook(xlfile)
+    
+    # create new sheet
+    print('test-registration(copy_to_new_sheet): Creating new sheet ' + tempsheet + ' in ' + xlfile)
+    wb.create_sheet(tempsheet)
+    
+    # get the sheet to load data from
+    temp_sheet = wb.get_sheet_by_name(tempsheet)
+    
+    # copy data
+    print('test-registration(copy_to_new_sheet): Copying data from ' + mastersheet + ' to ' + tempsheet + ' in ' + xlfile)
+    for row in wb [mastersheet] ['B3':'H14']:
+        for cell in row:
+            temp_sheet[cell.coordinate] = cell.value
+    
+    # save the workbook
+    wb.save('C:\\Users\\medwa\\Desktop\\xlfolder\\test_copy.xlsx')
 
-# create new sheet
-tempsheet = 'TEMPSHEET'
-print('test-registration: Creating new sheet ' + tempsheet + ' in ' + xlfile)
-wb.create_sheet(tempsheet)
-
-# get the sheet to load data from
-mastersheet = 'Sheet1'
-sheet = wb.get_sheet_by_name(mastersheet)
-
-# copy data
-print('test-registration: Copying data from ' + mastersheet + ' to ' + tempsheet + ' in ' + xlfile)
-temp_sheet = wb.get_sheet_by_name(tempsheet)
-for row in wb [mastersheet] ['B3':'H12']:
-    for cell in row:
-        # print(cell.coordinate, cell.value)
-        temp_sheet[cell.coordinate] = cell.value
-    #print('--- END OF ROW ---')
-
-# save the workbook
-wb.save('C:\\Users\\medwa\\Desktop\\xlfolder\\test_copy.xlsx')
+def match_and_log_user(qrcode, barcode, xlfile, sheet):
+    # split qrcode string
+    userinfo = qrcode.split(',')
+    
+    # load workbook
+    print('test-registration(match_and_log_user): Loading workbook ' + xlfile)
+    wb = openpyxl.load_workbook(xlfile)
+    
+    # get the sheet to match against
+    sheetobj = wb.get_sheet_by_name(sheet)
+    
+    # search sheet for matching userinfo
+    # make list of all users, then if comparison?
+    
+    
+# function calls
+# copy_to_new_sheet('C:\\Users\\medwa\\Desktop\\xlfolder\\test.xlsx', 'TEMPSHEET', 'Sheet1')
+match_and_log_user(qrcode, 'BR12345678', 'C:\\Users\\medwa\\Desktop\\xlfolder\\test.xlsx', 'TEMPSHEET')
